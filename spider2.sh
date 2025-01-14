7ed695441d2f1deb99ec918998db94641dc2b7d0
Amit Gajare <48202924+Amitgajare2@users.noreply.github.com>
2025-01-14 17:18:23 +0530

    spiderman logo

---
+++ spider2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Reset and Colors
+reset="\033[0m"
+red="\033[1;31m"
+blue="\033[1;34m"
+white="\033[1;37m"
+gray="\033[1;90m"
+
+# Spider-Man Logo and Tagline
+echo -e "
+${red}                ██████                
+${red}            ████      ████            
+${red}         ████   ${blue}██████${red}   ████         
+${red}      ████   ${blue}██████████${red}   ████      
+${red}    ████   ${blue}██████████████${red}   ████    
+${red}   ████   ${blue}████████████████${red}   ████   
+${red}  ████   ${blue}██████████████████${red}   ████  
+${red}  ████   ${blue}██████████████████${red}   ████  
+${red}   ████   ${blue}████████████████${red}   ████   
+${red}    ████   ${blue}██████████████${red}   ████    
+${red}      ████   ${blue}██████████${red}   ████      
+${red}         ████   ${blue}██████${red}   ████         
+${red}            ████      ████            
+${red}                ██████                
+
+${white}  ~ WEIRD THINGS HAPPEN TO ME ALL THE TIME. ~
+${gray} Heroes fight villains; programmers fight bugs.
+${reset}"
